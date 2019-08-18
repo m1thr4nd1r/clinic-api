602b2531f8967da96860f8127d5da4babbcbbdef
Victor <vitocr_santos@hotmail.com>
2019-08-18 12:34:39 -0300

    Saving schedules to local json

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -1,13 +1,33 @@
-exports.post = (req, res, next) => {
-    // TODO: implementar processamento do request
-    if (req.body.type == 'weekly')
-    {
-        let days = req.body.days;
-        let intervals = req.body.intervals;
-    }
+const db = require('../dao/clinicDB');
+const shortid = require('shortid')
 
-    res.status(201).send('Requisição recebida com sucesso!');
+exports.post = (req, res, next) => {
+    let type = req.body.type;
+    let days = req.body.days;
+    let intervals = req.body.intervals;
+    let id = null
+
+    if (type == 'weekly' || type == 'daily' || type == 'day')
+    {
+        id = shortid.generate()
+        let schedules = db.get('schedules');
+
+        schedules.push({
+            id: id,
+            type: type,
+            days: days,
+            intervals: intervals
+        }).write();
+        
+        db.get('count', n => n + 1)
+          .write()
+        
+        res.status(201).send(`Agendamento ${id} criada com sucesso!`);
+    }
+    else
+        res.status(400).send("Tipo incorreto de agendamento.")
 };
+
 exports.put = (req, res, next) => {
     let id = req.params.id;
     res.status(201).send(`Requisição recebida com sucesso! ${id}`);
