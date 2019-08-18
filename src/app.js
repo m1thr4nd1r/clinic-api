2cba0a7ac18b64802313b906150e2e62718b8653
Victor <vitocr_santos@hotmail.com>
2019-08-18 12:32:39 -0300

    Removes unused imports

--- src/app.js
+++ src/app.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
-const bodyParser = require('body-parser');
 
 //Rotas
 const index = require('./routes/index');
 const calendarRoute = require('./routes/calendarRoute');
-//app.use(bodyParser);
 app.use(express.json())
 app.use('/', index);
 app.use('/schedule', calendarRoute);
 module.exports = app;
-
-// const express = require('express');
-// const app = express();
-// const router = express.Router();
-// //Rotas
-// const index = require('./routes/index');
-// const personRoute = require('./routes/personRoute');
-// app.use('/', index);
-// app.use('/persons', personRoute);
-// module.exports = app;
