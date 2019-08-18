b91fe2df292c6217e0e483bc424fbf62f39e4881
Victor <vitocr_santos@hotmail.com>
2019-08-18 19:03:37 -0300

    Add 'check free space' route

--- src/routes/scheduleRoute.js
+++ src/routes/scheduleRoute.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/scheduleController')
 
+// Create schedule
 router.post('/', controller.post);
+
+// Check free space based on schedules
+router.get('/check/:start/:end', controller.check);
+// List schedules with id
 router.get('/:dev', controller.get);
+// List schedules
 router.get('/', controller.get);
+
+// Delete schedules
 router.delete('/:id', controller.delete);
 
 module.exports = router;
