9a64223246372df274e61c215d49487095b57f1f
Victor <vitocr_santos@hotmail.com>
2019-08-18 14:35:38 -0300

    Adds logic to delete schedules

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -35,7 +35,18 @@ exports.put = (req, res, next) => {
     let id = req.params.id;
     res.status(201).send(`Requisição recebida com sucesso! ${id}`);
 };
+
 exports.delete = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send(`Requisição recebida com sucesso! ${id}`);
+    let schedules = db.get('schedules');
+    let schedule = schedules.find({id: req.params.id}).value()    
+
+    if (schedule == undefined)
+        res.status(400).send('Agendamento não encontrado.');
+    else
+    {
+        schedules.remove({id: schedule.id})
+                 .write()
+
+        res.status(200).send(`Agendamento ${schedule.id} removido com sucesso!`);
+    }
 };
